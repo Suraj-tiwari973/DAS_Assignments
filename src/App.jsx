822be3885f45b8635c62b2cec665b9f500a65e91
Abhishek Tiwari <tiwarisuraj0852@gmail.com>
2023-06-02 17:15:18 +0530

    working of buttons on changing useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
-import Acheivements from './pages/Acheivements';
-import Education from './pages/Education'
-import Introduction from './pages/Intoduction';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Introduction/>
-  },
-  {
-    path: "/acheivements",
-    element: <Acheivements/>
-  },
-  {
-    path: "education",
-    element: <Education/>
-  },
-])
+import { useState,useEffect } from 'react'
 
 export default function App() {
+
+    const [count,setCount] = useState(0);
+    const [num,setNum] = useState(0);
+
+    const incr = ()=>{
+        setCount(count+1)
+    }
+    const dcr = ()=>{
+        setCount(count-1)
+    }
+    const incrNum = ()=>{
+        setNum(num+1);
+    }
+    const dcrNum = ()=>{
+        setNum(num-1);
+    }
+    useEffect(() => {
+        console.log("only num updated");
+    }, [num,count])
+    
   return (
-    <>
-    <div>
-      <RouterProvider router = {router}/>
+    <div style={{textAlign:"center"}}>
+        <h1>Counter App</h1>
+        <h2>Counter is: {count}</h2>
+        <h2>Num is: {num}</h2>
+        <button onClick={incr}>Increment count</button>
+        <button onClick={dcr}>Decrement count</button><br />
+        <button onClick={incrNum}>Incr Num</button>
+        <button onClick={dcrNum}>Dcr Num</button>
     </div>
-    </>
   )
 }
+
+
